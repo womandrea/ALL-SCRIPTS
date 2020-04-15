37423baf09b91e29d7a2523eeebe172d96e42ab7
womandrea <59843197+womandrea@users.noreply.github.com>
2020-04-15 18:55:32 -0400

    Update run_polish.py

--- run_polish.py
+++ run_polish.py
@@ -1,12 +1,12 @@
 import argparse
 import polisher
 
-parser = argparse.ArgumentParser(description="To be used for guppybase calling")
+parser = argparse.ArgumentParser(description="Assumes that corresponding assembly script was used for assembly of the genome. Polishing done with either short or long reads.")
 
 parser.add_argument("-illumina ", metavar='PATH',
-                    type=str, dest="shortdir", help='Directory with all short reads, if available')
+                    type=str, dest="shortdir", help='Directory with all short reads.')
 parser.add_argument("-minion", metavar="PATH",
-                    type=str, dest="longdir", help="Directory with output(s) from assembly.py.")
+                    type=str, dest="longdir", help="Directory with output(s) from Assembly.py")
 args = parser.parse_args()
 
 assmb = polish_run.InputArg(args)
