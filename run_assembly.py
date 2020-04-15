3240f9f9541a8e4474936c652f5f2879e6c3b045
womandrea <59843197+womandrea@users.noreply.github.com>
2020-04-15 18:38:07 -0400

    Update run_assembly.py
    Added some clarity to the user inputs

--- run_assembly.py
+++ run_assembly.py
@@ -2,23 +2,20 @@ import argparse
 import Assembler
 
 parser = argparse.ArgumentParser(description="RIGHT NOW: Will take an input directory and convert all the \
-fasta files either within the directory or within its subdirectories.")
+fasta files either within the directory or within its subdirectories.\n All outputs will be put in the directories where their reads belong.")
 
 parser.add_argument("-longreads",
-                    metavar='input path', type=str,
-                    dest="long", help="Path to directory.")
-parser.add_argument("-shasta_out",
-                    metavar="output directory", type=str,
-                    dest="output", help="Output directory for the fasta files.")  # DO NOT USE THIS PARAM, remove
+                    metavar='input_path', type=str,
+                    dest="long", help="Path to directory with all reads.", required=False)
 parser.add_argument("-minreads",
                     metavar="READ CUTOFF", type=str,
-                    dest="minr", help= "Minimum required read length")
+                    dest="minr", help= "Minimum required read length", default=False, required=True)
 parser.add_argument("-shortreads",
-                    metavar="FORWARD", type=str,
-                    dest="sr", help="Directory containing all short reads in fastq.gz files.")
-parser.add_argument("-type",
-                    metavar="[contig, nanpore, short]",
-                    type=str, dest="type", help="If we used SPAdes, what do we do with the long reads?")
+                    metavar="illumina_path", type=str,
+                    dest="sr", help="Directory containing all short reads in fastq.gz files.", required=Fase)
+parser.add_argument("-assembly_type",
+                    metavar="[contig, long, short]", required=True,
+                    type=str, dest="type", help="Contig corresponds to hybrid assembly, long to long read assembly, and short to read read assembly.")
 
 
 args = parser.parse_args()
