437b34005dfcb8ed9d4c44976218fafbbbd2320c
womandrea <59843197+womandrea@users.noreply.github.com>
2020-04-15 18:52:21 -0400

    Update run_filtering.py

--- run_filtering.py
+++ run_filtering.py
@@ -1,12 +1,12 @@
 import filtering
 import argparse
 
-parser = argparse.ArgumentParser(description="Providing long reads or short reads, filters reads for quality")
-parser.add_argument("-longreads", metavar="DIR PATH", dest="LR", type=str,help="Default None")
-parser.add_argument("-shortreads", metavar="DIR PATH", dest="SR", type=str, help="Default None")
-parser.add_argument("-genomesize", metavar="NUM", dest="genomesize", help="Use integer values for size of genome. Default None.")
-parser.add_argument("-filter", metavar="Y/N", dest="shortreads", help="Process short reads with bbduk for filtlong?")
-parser.add_argument("-minlen", metavar="NUM", dest="minlen", help="filtlong only: what is the min length ")
+parser = argparse.ArgumentParser(description="Providing long reads or short reads, filters reads for quality.")
+parser.add_argument("-longreads", metavar="DIR PATH", dest="LR", type=str,help="Full file path to directory with long reads.", default=False)
+parser.add_argument("-shortreads", metavar="DIR PATH", dest="SR", type=str, help="ull file path to directory with Illumina reads", default=False)
+parser.add_argument("-genomesize", metavar="NUM", dest="genomesize", help="Use integer values for size of genome.", default=False)
+parser.add_argument("-filter", action="store_true", default=False, dest="shortreads", help="Specify if short reads should be filtered with bbduk before acting as a reference for Filtlong.")
+parser.add_argument("-minlen", metavar="NUM", dest="minlen", help="For Filtlong only: minimum length of reads to filter.")
 
 args = parser.parse_args()
 
